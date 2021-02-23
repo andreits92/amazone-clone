e97e9e2fd6a665b7a84769258fa5389abc5f039f
andreits92 <67470981+andreits92@users.noreply.github.com>
2021-02-23 21:12:58 +0200

    nav and home

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDoZ3V38xE8iGwDoZ8jkWD_qtV_jf79F1I",
+  authDomain: "clone-b6d2d.firebaseapp.com",
+  projectId: "clone-b6d2d",
+  storageBucket: "clone-b6d2d.appspot.com",
+  messagingSenderId: "315025238444",
+  appId: "1:315025238444:web:9e3d9194b5efb746c17209",
+  measurementId: "G-GVVBT0MENP",
+};
